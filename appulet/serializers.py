dc8dd900e59a4b0871d6f0e8fde5e1910a82a2e8
John Palmer <johnrbpalmer@gmail.com>
2014-10-16 19:34:18 +0200

    changed id to server_id in api

--- appulet/serializers.py
+++ appulet/serializers.py
@@ -11,26 +11,35 @@ class MapSerializer(serializers.ModelSerializer):
 
 
 class HighlightSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
+
     class Meta:
         model = Highlight
 
 
 class BoxSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
+
     class Meta:
         model = Box
 
 
 class ReferenceSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
+
     class Meta:
         model = Reference
 
 
 class InteractiveImageSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
+
     class Meta:
         model = InteractiveImage
 
 
 class InteractiveImageNestedSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
     boxes = BoxSerializer(many=True)
     original_height = serializers.Field()
     original_width = serializers.Field()
@@ -38,50 +47,58 @@ class InteractiveImageNestedSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = InteractiveImage
-        fields = ('id', 'image_file', 'image_name', 'original_height', 'original_width', 'boxes')
+        fields = ('server_id', 'image_file', 'image_name', 'original_height', 'original_width', 'boxes')
 
 
 class HighlightNestedSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
     interactive_images = InteractiveImageNestedSerializer(many=True)
     references = ReferenceSerializer(many=True)
     media_name = serializers.Field()
 
     class Meta:
         model = Highlight
-        fields = ('id', 'created_by', 'name_oc', 'name_es', 'name_ca', 'name_fr', 'name_en', 'long_text_oc', 'long_text_es', 'long_text_ca', 'long_text_fr', 'long_text_en', 'radius', 'type', 'interactive_images', 'references', 'media_name')
+        fields = ('server_id', 'created_by', 'name_oc', 'name_es', 'name_ca', 'name_fr', 'name_en', 'long_text_oc', 'long_text_es', 'long_text_ca', 'long_text_fr', 'long_text_en', 'radius', 'type', 'interactive_images', 'references', 'media_name')
 
 
 class StepSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
 
     class Meta:
         model = Step
 
 
 class StepNestedSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
     highlights = HighlightNestedSerializer(many=True)
 
     class Meta:
         model = Step
-        fields = ('id', 'absolute_time', 'order', 'latitude', 'longitude', 'altitude', 'precision', 'highlights')
+        fields = ('server_id', 'absolute_time', 'order', 'latitude', 'longitude', 'altitude', 'precision', 'highlights')
 
 
 class TrackSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
+
     class Meta:
         model = Track
 
 
 class TrackNestedSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
     steps = StepNestedSerializer(many=True)
 
     class Meta:
         model = Track
-        fields = ('id', 'name_oc', 'name_es', 'name_ca', 'name_fr', 'name_en', 'steps')
+        fields = ('server_id', 'name_oc', 'name_es', 'name_ca', 'name_fr', 'name_en', 'steps')
 
 
 class RouteSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
+
     class Meta:
         model = Route
-        fields = ('id', 'official', 'last_modified')
+        fields = ('server_id', 'official', 'last_modified')
 
 
 class RouteNestedSerializer(serializers.ModelSerializer):
@@ -89,13 +106,16 @@ class RouteNestedSerializer(serializers.ModelSerializer):
     reference = ReferenceSerializer(many=False)
     map = MapSerializer(many=False)
     created_by = serializers.RelatedField(many=False)
+    server_id = serializers.IntegerField(source='id')
 
     class Meta:
         model = Route
-        fields = ('id', 'official', 'id_route_based_on', 'created_by', 'map', 'description_oc', 'description_es', 'description_ca', 'description_fr', 'description_en', 'short_description_oc', 'short_description_es', 'short_description_ca', 'short_description_fr', 'short_description_en', 'name_oc', 'name_es', 'name_ca', 'name_fr', 'name_en', 'reference', 'track', 'created', 'last_modified')
+        fields = ('server_id', 'official', 'id_route_based_on', 'created_by', 'map', 'description_oc', 'description_es', 'description_ca', 'description_fr', 'description_en', 'short_description_oc', 'short_description_es', 'short_description_ca', 'short_description_fr', 'short_description_en', 'name_oc', 'name_es', 'name_ca', 'name_fr', 'name_en', 'reference', 'track', 'created', 'last_modified')
 
 
 class RatingSerializer(serializers.ModelSerializer):
+    server_id = serializers.IntegerField(source='id')
+
     class Meta:
         model = Rating
 
