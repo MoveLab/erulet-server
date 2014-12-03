13fe2b5277924be8a2a7203ec56089599f636ac1
John Palmer <johnrbpalmer@gmail.com>
2014-12-03 19:01:58 +0100

    media name added back to top 5 serializer

--- appulet/serializers.py
+++ appulet/serializers.py
@@ -127,6 +127,7 @@ class StepSerializer(serializers.ModelSerializer):
 
 class UserTopFiveHighlightNestedSerializer(serializers.ModelSerializer):
     server_id = serializers.IntegerField(source='id', read_only=True)
+    media_name = serializers.Field()
     media_url = serializers.Field()
     average_rating = serializers.Field()
     total_ratings = serializers.Field()
@@ -134,7 +135,7 @@ class UserTopFiveHighlightNestedSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Highlight
-        fields = ('server_id', 'average_rating', 'total_ratings', 'created_by', 'name_oc', 'name_es', 'name_ca', 'name_fr', 'name_en', 'long_text_oc', 'long_text_es', 'long_text_ca', 'long_text_fr', 'long_text_en', 'radius', 'type', 'media_url', 'step')
+        fields = ('server_id', 'average_rating', 'total_ratings', 'created_by', 'name_oc', 'name_es', 'name_ca', 'name_fr', 'name_en', 'long_text_oc', 'long_text_es', 'long_text_ca', 'long_text_fr', 'long_text_en', 'radius', 'type', 'media_name', 'media_url', 'step')
 
 
 class StepNestedSerializer(serializers.ModelSerializer):
