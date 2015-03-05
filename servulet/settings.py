a70482c862770d9c8d6ce9e0d7a24072345a716d
John Palmer <johnrbpalmer@gmail.com>
2015-03-05 10:58:33 +0100

    changed order of authentications so token comes first

--- servulet/settings.py
+++ servulet/settings.py
@@ -144,8 +144,8 @@ MEDIA_ROOT = ''
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
